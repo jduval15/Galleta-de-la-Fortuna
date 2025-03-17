7df2b3ff9daf9e94ca694d54b5c3742dd818d786
Jose Luis Duval Perez <joseluis_duval@hotmail.com>
2025-03-17 12:28:01 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+import phrases from './utils/phrases'
+import bgArr from './utils/bgArr'
+import { useState } from 'react'
+import getRandomFromArr from './services/getRandomFromArr'
+import Phrase from './components/Phrases'
+import ButtonPhrase from './components/ButtonPhrases'
+
+
+
+
+function App() {
+
+  const quote = getRandomFromArr(phrases)
+
+  const [phraseRandom, setPhraseRandom] = useState(quote)
+  const [bgApp, setBgApp] = useState(getRandomFromArr(bgArr))
+
+  const objStyle = {
+    backgroundImage: `url('../fondo${bgApp}.jpg')`
+    
+  }
+  
+  
+  
+  return (
+    <div style={objStyle} className='app'>
+      <h1 className='app__tittle'>Fortune Cookie</h1>
+      <article className='app__card'>
+      <Phrase 
+      phraseRandom = {phraseRandom}
+      />
+      < ButtonPhrase
+      setPhraseRandom = {setPhraseRandom}
+      setBgApp = {setBgApp}
+      />
+      </article>
+    </div>
+    
+    
+  )
+}
+
+export default App
